10cc36943ee0417c5542e387c338e9cadb4db40e
T107 <T107@PC259>
2014-11-10 19:43:35 -0600

    Excepciones

---
+++ EXCEPCIONES/src/excepciones/Excepciones1.java
@@ -0,0 +1,15 @@
+
+package excepciones;
+
+
+public class Excepciones1 {
+
+    
+    public static void main(String[] args) {
+      int x=2;
+      float y=0;
+      
+      System.out.println(x/y);
+    }
+    
+}
